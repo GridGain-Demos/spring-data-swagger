b5794cc668c2fa5ce44a03ca93b4a02969ed9e93
Stephen Darlington <stephen.darlington@gridgain.com>
2024-09-03 14:58:44 +0100

    Switch to OpenAPI from SpringFox/Swagger

--- src/main/java/com/gridgain/training/spring/IgniteConfig.java
+++ src/main/java/com/gridgain/training/spring/IgniteConfig.java
@@ -1,29 +1,29 @@
 package com.gridgain.training.spring;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.apache.ignite.Ignite;
-import org.apache.ignite.configuration.ClientConnectorConfiguration;
-import org.apache.ignite.configuration.ConnectorConfiguration;
 import org.apache.ignite.springframework.boot.autoconfigure.IgniteConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.RequestHandler;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-@EnableSwagger2
-
-public class IgniteConfig {
+public class IgniteConfig extends WebMvcConfigurationSupport {
     @Bean(name = "igniteInstance")
     public Ignite igniteInstance(Ignite ignite) {
         return ignite;
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry)
+    {
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/5.11.8/");
+    }
     @Bean
     public IgniteConfigurer configurer() {
         return igniteConfiguration -> {
@@ -32,14 +32,17 @@ public class IgniteConfig {
     }
 
     @Bean
-    public Docket apiDocket(){
-        Docket docket = new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.gridgain.training.spring"))
-                .paths(PathSelectors.any())
-                .build();
-        return docket;
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("SpringData Swagger")
+                        .description("SpringData + REST + SwaggerUI")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringData Swagger Documentation")
+                        .url("https://github.com/rdGridGain/spring-data-swagger"));
     }
+
 
 }
 
