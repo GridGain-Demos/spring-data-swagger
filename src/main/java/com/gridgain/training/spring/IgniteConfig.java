8c039b36c51060b7ff62e38bdb8d7e99ff7ad516
Stephen Darlington <stephen.darlington@gridgain.com>
2024-09-03 15:05:09 +0100

    Switch to GridGain thin client

--- src/main/java/com/gridgain/training/spring/IgniteConfig.java
+++ src/main/java/com/gridgain/training/spring/IgniteConfig.java
@@ -4,8 +4,6 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import org.apache.ignite.Ignite;
-import org.apache.ignite.springframework.boot.autoconfigure.IgniteConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -13,22 +11,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 
 @Configuration
 public class IgniteConfig extends WebMvcConfigurationSupport {
-    @Bean(name = "igniteInstance")
-    public Ignite igniteInstance(Ignite ignite) {
-        return ignite;
-    }
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry)
     {
         registry.addResourceHandler("/swagger-ui/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/5.11.8/");
-    }
-    @Bean
-    public IgniteConfigurer configurer() {
-        return igniteConfiguration -> {
-            igniteConfiguration.setClientMode(true);
-        };
     }
 
     @Bean
